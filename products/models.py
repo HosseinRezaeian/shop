fe0692a701b778232237e6fd8eb05f091e9b353c
hossain rezaeian <hossainrezaeian@gmail.com>
2022-07-03 13:09:37 +0430

    Version 1

--- products/models.py
+++ products/models.py
@@ -4,14 +4,16 @@ from django.db import models
 class category(models.Model):
     title = models.CharField(max_length=30)
     slug = models.SlugField(max_length=30)
+
     def __str__(self):
         return self.title
+
 
 # Create your models here.
 class product(models.Model):
     pic = models.ImageField(upload_to='image', null=True, blank=True)
     title = models.CharField(max_length=30)
-    category = models.ManyToManyField(category)
+    category = models.ManyToManyField(category, related_name='category')
     price = models.IntegerField(max_length=11)
     discripion = models.CharField(max_length=300)
     slug = models.SlugField(max_length=30)
--- products/views.py
+++ products/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 
+from .models import category
 from .models import product
 
 
-# Create your views here.
+# # Create your views here.
+def prodact(request, ps):
+    f = category.objects.get(slug=ps)
+    c = f.category.all()
+
+    return render(request, 'pro.html', {'pr': c})
+
+
 def index(request):
-    c = product.objects.all()
+    c = category.objects.all()
 
     return render(request, 'index.html', {'pr': c})
