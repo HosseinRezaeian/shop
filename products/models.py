cb349cdb9f69c201ace7e916d96a03a3c5c62e38
hossain rezaeian <hossainrezaeian@gmail.com>
2022-07-03 10:39:40 +0430

    finish backend

--- products/models.py
+++ products/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+
+class category(models.Model):
+    title = models.CharField(max_length=30)
+    slug = models.SlugField(max_length=30)
+    def __str__(self):
+        return self.title
+
 # Create your models here.
+class product(models.Model):
+    pic = models.ImageField(upload_to='image', null=True, blank=True)
+    title = models.CharField(max_length=30)
+    category = models.ManyToManyField(category)
+    price = models.IntegerField(max_length=11)
+    discripion = models.CharField(max_length=300)
+    slug = models.SlugField(max_length=30)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
--- products/views.py
+++ products/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
+from .models import product
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    c = product.objects.all()
+
+    return render(request, 'index.html', {'pr': c})
